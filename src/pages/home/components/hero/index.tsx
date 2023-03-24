ea07772525185916d3e79591fed772a6f2522d64
Icaro Andries <icaro.andries15@gmail.com>
2023-03-24 17:39:11 -0300

    feat: subindo alias

--- src/pages/home/components/hero/index.tsx
+++ src/pages/home/components/hero/index.tsx
@@ -1,27 +1,27 @@
 import './style.sass'
-import img1 from '../../../../../public/imgs/corgi1.jpg';
-import img2 from '../../../../../public/imgs/corgi2.jpg';
-import img3 from '../../../../../public/imgs/corgiCarente.jpg';
-import img4 from '../../../../../public/imgs/corgiVesgo.jpg';
+import img1 from '@images/corgi1.jpg';
+import img2 from '@images/corgi2.jpg';
+import img3 from '@images/corgiCarente.jpg';
+import img4 from '@images/corgiVesgo.jpg';
 
 function Hero() {
   return (
     <div className='main-container'>
         <div className='feature image-1'>
             <img src={img1} alt="igão" />
-            <caption>corgi1</caption>
+            <p>corgi1</p>
         </div>
         <div className='feature image-2'>
             <img src={img2} alt="igão" />
-            <caption>corgi2</caption>
+            <p>corgi2</p>
         </div>
         <div className='feature image-3'>
             <img src={img3} alt="igão" />
-            <caption>corgi3</caption>
+            <p>corgi3</p>
         </div>
         <div className='feature image-4'>
             <img src={img4} alt="igão" />
-            <caption>corgi4</caption>
+            <p>corgi4</p>
         </div>
     </div>
   )
