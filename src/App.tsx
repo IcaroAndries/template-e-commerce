d9c10a3b42ab5fa962577a6439511e246e526166
Icaro Andries <icaro.andries15@gmail.com>
2023-03-26 21:28:52 -0300

    fix: corrigindo erro no vite

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,17 @@ import './App.sass'
 import Header from './components/header'
 import Home from './pages/home';
 import Products from './pages/products';
-import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom'; 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
 
 function App() {
 
   return (
     <div className='container'>
+      <Header />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Header/>}>
-            <Route path="/home" element={<Home />}/> 
-            <Route path="/products" element={<Products />}/>
-            <Outlet />
-          </Route>
+          <Route path="/" element={<Home />}/> 
+          <Route path="/products" element={<Products />}/>
         </Routes>
       </ BrowserRouter>
     </div>
